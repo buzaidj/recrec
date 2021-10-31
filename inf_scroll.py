f7116a13660515fb8baf706d0f6680f51f762fa0
maspin22 <maspinwall22@gmail.com>
2021-10-31 19:40:33 -0400

    more class improvements

--- inf_scroll.py
+++ inf_scroll.py
@@ -1,3 +1,8 @@
+import urllib.request
+from PIL import Image
+from abc import ABC, abstractmethod
+  
+
 class Feed:
     def __init__(self, userdb, recidb):
         self.userdb = userdb
@@ -7,35 +12,36 @@ class Feed:
         pass
 
     def display(self, rec):
-        pass
+        ph_url = rec.get("photo_url")
+        urllib.request.urlretrieve(ph_url,"f.png")
+  
+        img = Image.open("f.png")
+        img.show()
 
     def save_and_pickle(self, rec, user_meta_json):
         pass
 
-
-    def start(self, p, d):
+    def start(self):
         print("Welcome to your favorite recipe recommender RecRec: \n" )
-
         
         #returns name of user_meta_json or empty if not used before
         user_meta_json = self.userdb.get(input("What is you name? \n"))
         #eg. svm, percep, nn...
         rectype = input("What kind of recommeder would you like to use? \n")
-                
-        
+                       
         recom = Recommender(rectype, user_meta_json, bool(user_meta_json), self.recidb)
 
         if user_meta_json :
             print("Training your recommender from prior prefrences")
-            recom.train(self, numrecs, user_meta_json)
+            recom.train(self, user_meta_json)
 
         print("All ready!")
         
         con = True
-        while con
+        while con:
             rec = recom.recommend(1) 
             self.display(rec)
-            user_res = input("Would you cook this? y/n")
+            user_pref = input("Would you cook this? y/n")
             user_meta_json = self.update_user_json(user_meta_json, rec, user_pref)
             con = not (input("Would you like to quit? q") == 'q')
             recom.train(self, user_meta_json)
@@ -48,11 +54,11 @@ class Feed:
         
 
 
-class Recommender:
-    def __init__(self, rectype, userjson, hasjson):
+class Recommender(ABC):
+    def __init__(self, rectype, userjson, recjson):
         self.rectype = rectype
-        self.userdb = userdb
-        self.recidb = recidb
+        self.userdb = userjson
+        self.recidb = recjson
 
     @abstractmethod
     def train(self, user_meta_json):
---
+++ randRecommender.py
@@ -0,0 +1,11 @@
+class ranRecommender(Recommender):
+    def __init__(self, rectype, userjson, recjson):
+        super().__init__(rectype, userjson, recjson)
+
+    def train(self, user_meta_json):
+        pass
+        
+    def recommend(self, numrecs):
+        
+        self.recjson
+        pass
