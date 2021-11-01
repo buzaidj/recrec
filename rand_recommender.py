c8ed48194249e43ebeb6a91d38bfaff282da1e9a
James Buzaid <jjb422@cornell.edu>
2021-10-31 20:38:52 -0400

    rand_recom

--- rand_recommender.py
+++ rand_recommender.py
@@ -1,11 +1,18 @@
+from typing_extensions import TypeAlias
+from process import Recommender
+from typing import List
+from random import choice
+
+Recipe = dict
+
+
 class RandRecommender(Recommender):
-    def __init__(self, rectype, userjson, recjson):
-        super().__init__(rectype, userjson, recjson)
+    def __init__(self, rectype, userjson, recipes: List[Recipe]):
+        super().__init__(rectype, userjson, recipes)
 
     def train(self, user_meta_json):
+        """ just selects a random recipe over all recipes, no training """
         pass
-        
-    def recommend(self, numrecs):
-        
-        self.recjson
-        pass
+
+    def recommend(self, numrecs) -> Recipe:
+        return choice(self.recipes, k=numrecs)
