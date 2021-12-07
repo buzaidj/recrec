66c6f80c99d0ef85f785ec11e3a10c476318bdbd
buzaidj <66746824+buzaidj@users.noreply.github.com>
2021-12-06 20:49:12 -0500

    Add files via upload

--- time.py
+++ time.py
@@ -5,38 +5,43 @@ from typing import Dict
 
 data = []
 
-"""
-reads in data_file line by line and removes adjacent recipes, writing to new_file
-"""
-def time() -> None:
-    json_name = "./mit_5_recipes"
+
+def add_time_data(data_file) -> None:
+    """
+    reads in data_file line by line and removes adjacent recipes, writing to new_file
+    """
+
     recipes = None
-    with open(json_name + '.json') as recp:
+    with open(data_file + '.json') as recp:
         recipes = json.load(recp)
         for i in recipes:
             idle_time = 0
             for j in i['instructions']:
                 line = j['text'].split()
+                idle_time_pre_upd = idle_time
                 for k in range(len(line)):
-                    word = re.sub(r'[^\w\s]','', line[k]).lower()
+                    word = re.sub(r'[^\w\s]', '', line[k]).lower()
                     if word == "hour" or word == "hours":
                         try:
-                            idle_time+=int(line[k-1]) * 60
+                            idle_time += int(line[k-1]) * 60
                         except:
-                            break;
+                            pass
                     elif word == "minutes" or word == "minute":
                         try:
-                            idle_time+=int(line[k-1])
+                            idle_time += int(line[k-1].split('-')[0])
                         except:
-                            break;
-            
+                            pass
+
+                # if no time for instruction given, assume 2 minutes
+                if idle_time_pre_upd == idle_time:
+                    idle_time += 2
+
             i['idle_time'] = idle_time
             data.append(i)
 
-
             # if recipes are equal, keep last data the same
-    with open("new_file.json", 'w') as f_new:
+    with open(data_file + '_time' + ".json", 'w') as f_new:
         f_new.write(json.dumps(data))
 
-time()
 
+add_time_data('mit_5_recipes_prices')
