8d82619bfec8a62b90f0e72afbf0ec01a619628e
James Buzaid <jjb422@cornell.edu>
2021-12-08 20:47:48 -0500

    working on uii

--- inf_scroll.py
+++ inf_scroll.py
@@ -11,21 +11,19 @@ import csv
 import json
 import ast
 
-  
 
 class Feed:
-    def __init__(self, recis):
-        self.reci_file = recis
+    def __init__(self, recipes_filename):
+        self.reci_df = pd.read_csv(recipes_filename)
 
     def update_user_pref(self, user_pref_file_name, rec, pref):
         with open(user_pref_file_name, 'w', newline='') as csvfile:
-            
 
     def display(self, rec):
+        raise NotImplemented
         # print(rec["title"])
 
         # # print("Cook time in minutes: " + str(rec["total_time_minutes"]))
-
 
         # print("You will need, ingredients:")
         # for ing in rec["ingredients"]:
@@ -34,15 +32,18 @@ class Feed:
         # print("Instructions:")
         # for ins in rec["instructions"]:
         #     print(ins["text"])
-        
+
         # # ph_url = rec.get("photo_url")
         # # urllib.request.urlretrieve(ph_url,"f.png")
-  
+
         # # img = Image.open("f.png")
         # # img.show()
 
-    def rectype_to_rec(self, rectype, user_pref):
-        return RandRecommender(user_pref, self.recilist)
+    def rectype_to_rec(self, rectype):
+        """
+        return a Recommender from the inputted [rectype] string
+        """
+        return RandRecommender()
 
     def save(self, user_file, userpref):
         with open(user_file, 'w') as f:
@@ -56,46 +57,63 @@ class Feed:
         return pref
 
     def parse_user_csv(self, user_pref_file_name):
+        df = pd.read_csv('user_james.csv', index_col=0, header=False)
+        d = {}
+        for k, v in df.to_dict('list').items():
+            d[k] = v[0]
+        return d
 
-    def start(self):
-        print("Welcome to your favorite recipe recommender RecRec: \n" )
-        
+    def create_train_data(self):
+
+    def startup(self):
         name = input("What is your name? \n").strip().lower()
         user_pref_file_name = "user_" + name + ".csv"
+        recs_file_name = "recs_" + name + ".csv"
 
-        file_exists = exists(user_pref_file_name)
+        file_exists = exists(user_pref_file_name) and exists(recs_file_name)
         if file_exists:
             user_pref = parse_user_csv(user_pref_file_name)
+            prior_recs = parse_user_csv(recs_file_name)
         else:
-            user_pref = []
+            user_pref = {}
+            prior_recs = {}
 
-        #eg. svm, percep, nn...
+            with open(user_pref_file_name, 'a'):
+                pass
+            with open(recs_file_name, 'a'):
+                pass
+
+    def start(self):
+        print("Welcome to your favorite recipe recommender RecRec: \n")
+
+        # eg. svm, percep, nn...
         rectype = input("What kind of recommeder would you like to use? \n")
-                       
+
         recom = self.rectype_to_rec(rectype, user_pref)
+
+        # TODO:
 
         print("Training your recommender from prior prefrences")
         recom.train(user_pref)
 
         print("All ready!")
-        
+
         con = True
         while con:
-            rec = recom.recommend(1) 
+            rec = recom.recommend(1)
             self.display(rec[0])
             pref = self.user_pref()
             userpref = self.update_user_pref(user_pref_file_name, rec[0], pref)
             con = not (input("Would you like to quit? (q/n) : \n") == 'q')
             recom.train(userpref)
-            
 
         print("Saving your prefrences")
         self.save(user_pref_file_name, userpref)
 
         print("See you soon")
-        
+
 
 if __name__ == "__main__":
-    recis  = "recipes_with_nutritional_info_prices_time.csv"
+    recis = "recipes_with_nutritional_info_prices_time.csv"
     f = Feed(recis)
     f.start()
